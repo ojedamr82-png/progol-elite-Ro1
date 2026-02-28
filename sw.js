74b49feb42ef06162d72a12b3817fbc21b0c7a66
Rafael Ojeda Meza <ojedamr82@gmail.com>
2026-02-28 10:18:19 -0700

    Add service worker for caching static assets

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'progol-elite-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(STATIC_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) return response;
+        return fetch(event.request);
+      })
+  );
+});
